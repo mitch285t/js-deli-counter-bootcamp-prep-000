c4c9cf8cc926e015f5d3dfd275cdb766dcb709b4
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-24 17:27:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
  katzDeliLine.push(name)  
-   console.log("Welcome, "+name+". You are number "+line.length+" in line.")
-   return("Welcome, "+name+". You are number "+line.length+" in line.")
+   console.log("Welcome, "+name+". You are number "+katzDeliLine.length+" in line.")
+   return("Welcome, "+name+". You are number "+katzDeliLine.length+" in line.")
 }
