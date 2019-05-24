88034f80afdb0d2a6f93c5148ffba81e3ab477b3
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-24 17:48:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,8 @@ return "There is nobody waiting to be served!"
 }}
 
 function currentLine(katzDeliLine){
-  
+  if (!katzDeliLine.length){
+    console.log("The line is currently empty")
+    return "The line is currently empty"
+  }
 }
