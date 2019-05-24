88bd7ec057968656773c2a8f4b169c9051f33ca8
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-24 17:26:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var katzDeliLine = [];
 
 function takeANumber(Line, name){
+  line.push(name)  
    console.log("Welcome, "+name+". You are number "+line.length+" in line.")
    return("Welcome, "+name+". You are number "+line.length+" in line.")
 }
