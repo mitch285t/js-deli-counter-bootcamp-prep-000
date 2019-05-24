94b32bfc71d679f00fa0e32c8a7a022a63021eda
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-24 17:46:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function nowServing(katzDeliLine){
 console.log("There is nobody waiting to be served!")
 return "There is nobody waiting to be served!"
 } else {
-  return "currently serving "+ katzDeliLine.shift()+"."
+  return "Currently serving "+ katzDeliLine.shift()+"."
 }}
 
