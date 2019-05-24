2d217d41735d0b25a9ac1ee11fa2b4ca29448298
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-24 17:54:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(katzDeliLine){
   }
   var linenames = [];
   for (var i = 0;i < katzDeliLine.length; i++){
-    linenames.push(i+1 +". " + katzDeliLine[1]);
+    linenames.push(i+1 +". " + katzDeliLine[i]);
     console.log("The line is currently:" + linenames)
     return "The line is currently:" + linenames.join(",");
   }
