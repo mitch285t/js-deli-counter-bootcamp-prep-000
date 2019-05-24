259cb1cb9ebd709c62bdffd5af26646afcb21d04
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-24 17:23:53 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+function takeANumber(katzDeliLine, name){
+   for (let i = 1; i = i+1; i++){
+     katzDeliLine.push("Welcome, "+name+". You are number "+i+" in line.")
+   }
+   return katzDeliLine
+   
+}
